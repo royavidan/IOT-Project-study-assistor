3bef7eb4044c448da08b98f3574b561f22680d97
Roy Avidan <royavi1403@gmail.com>
2026-06-08 23:23:29 +0300

    made nice for VSCode also

---
+++ code.ino
@@ -0,0 +1,29 @@
+#include <Adafruit_VL6180X.h>
+
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+
+#include <Adafruit_SH110X.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_GrayOLED.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <Adafruit_SPITFT.h>
+#include <gfxfont.h>
+
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_GenericDevice.h>
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_I2CRegister.h>
+#include <Adafruit_SPIDevice.h>
+
+#include "Wire.h"
+
+
+void setup() {
+    
+}
+
+void loop() {
+    
+}
